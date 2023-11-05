78dade1f0b25267588abe06b24a500bc9edf36a3
desertagent <buiduyanh0@gmail.com>
2023-11-05 23:15:39 +0700

    Add web server config

---
+++ src/components/Game.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import PACK_OF_CARDS from '../utils/packOfCards'
+import shuffleArray from '../utils/shuffleArray'
+import io from 'socket.io-client'
+import queryString from 'query-string'
+import Spinner from './Spinner'
+import useSound from 'use-sound'
+
+import bgMusic from '../assets/sounds/game-bg-music.mp3'
+import unoSound from '../assets/sounds/uno-sound.mp3'
+import shufflingSound from '../assets/sounds/shuffling-cards-1.mp3'
+import skipCardSound from '../assets/sounds/skip-sound.mp3'
+import draw2CardSound from '../assets/sounds/draw2-sound.mp3'
+import wildCardSound from '../assets/sounds/wild-sound.mp3'
+import draw4CardSound from '../assets/sounds/draw4-sound.mp3'
+import gameOverSound from '../assets/sounds/game-over-sound.mp3'
+
+//NUMBER CODES FOR ACTION CARDS
+//SKIP - 404
+//DRAW 2 - 252
+//WILD - 300
+//DRAW 4 WILD - 600
+
+let socket
+// const ENDPOINT = 'http://localhost:5000'
+const ENDPOINT = 'https://uno-online-multiplayer.herokuapp.com/'
+
+const Game = (props) => {
+    const data = queryString.parse(props.location.search)
+
+    //initialize socket state
+    const [room, setRoom] = useState(data.roomCode)
+    const [roomFull, setRoomFull] = useState(false)
+    const [users, setUsers] = useState([])
+    const [currentUser, setCurrentUser] = useState('')
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        const connectionOptions =  {
+            "forceNew" : true,
+            "reconnectionAttempts": "Infinity", 
+            "timeout" : 10000,                  
+            "transports" : ["websocket"]
+        }
+        socket = io.connect(ENDPOINT, connectionOptions)
+
+        socket.emit('join', {room: room}, (error) => {
+            if(error)
+                setRoomFull(true)
+        })
+
+        //cleanup on component unmount
+        return function cleanup() {
+            socket.emit('disconnect')
+            //shut down connnection instance
+            socket.off()
+        }
+    }, [])
+
+  return (
+    <div>Game</div>
+  )
+}
+
+export default Game
